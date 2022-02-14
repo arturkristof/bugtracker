219417dfbd33a6c6500ec195de34d50a5fd94abf
Artur Kristof <artur.kristof@gmail.com>
2022-02-14 01:36:13 +0100

    Created Webpack hello world in frontend module

---
+++ frontend/src/main/frontend/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, '../../../target/frontend'),
+        clean: true,
+    },
+};
