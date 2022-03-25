83dc29ffede37e3eaae5d00715344ae509470c6e
Artur Kristof <artur.kristof@gmail.com>
2022-03-25 21:37:52 +0100

    Fixed compiling tex for correct table of contents

--- docs/compile.bat
+++ docs/compile.bat
@@ -1,4 +1,4 @@
 @RD /S /Q target
 for %%i in (*.tex) do (
-    if %%i NEQ doc-template.tex pdflatex %%i -output-directory=target -aux-directory=target\auxiliary
+    if %%i NEQ doc-template.tex pdflatex %%i
 )
